aca8655b958345d1d74e0ac1e264e8980d72c9e8
Maurits Lawende <mauritsl@243897.no-reply.drupal.org>
2010-10-27 20:04:45 +0000

    #954890 by Rolf van de Krol: Cleanup javascript

--- rate.js
+++ rate.js
@@ -1,33 +1,37 @@
 Drupal.behaviors.rate = function(context) {
-  $('a.rate-button', context).click(function() {
-
-    // Reveal the widget id.
-    $('#' + this.id).parents('div.rate-widget').each(function() {
-      widget = this.id;
+  $('.rate-widget:not(.rate-processed)', context).addClass('rate-processed').each(function () {
+    var widget = $(this);
+    var ids = widget.attr('id').match(/^rate\-([a-z]+)\-([0-9]+)\-([0-9]+)\-([0-9])$/);
+    var data = {
+      content_type: ids[1],
+      content_id: ids[2],
+      widget_id: ids[3],
+      widget_mode: ids[4]
+    };
+    
+    $('a.rate-button', widget).click(function() {
+      var token = this.getAttribute('href').match(/rate\=([a-f0-9]{32})/)[1];
+    
+      $.get(Drupal.settings.basePath + 'rate/vote/js?widget_id=' + data.widget_id + '&content_type=' + data.content_type + '&content_id=' + data.content_id + '&widget_mode=' + data.widget_mode + '&token=' + token + '&destination=' + escape(document.location), function(data) {
+        if (data.match(/^https?\:\/\/[^\/]+\/(.*)$/)) {
+          // We got a redirect.
+          document.location = data;
+        }
+        else {
+          // get parent object
+          var p = widget.parent();
+          
+          widget.before(data);
+          
+          // remove widget
+          widget.remove();
+          widget = undefined;
+          
+          Drupal.attachBehaviors(p.get(0));
+        }
+      });
+      
+      return false;
     });
-
-    // Extract required variables using regexp.
-    ids = this.getAttribute('href').match(/rate\=([a-f0-9]{32})/);
-    token = ids[1];
-    ids = widget.match(/^rate\-([a-z]+)\-([0-9]+)\-([0-9]+)\-([0-9])$/);
-    content_type = ids[1];
-    content_id = ids[2];
-    widget_id = ids[3];
-    widget_mode = ids[4];
-
-    // Request new widget HTML.
-    $.get(Drupal.settings.basePath + 'rate/vote/js?widget_id=' + widget_id + '&content_type=' + content_type + '&content_id=' + content_id + '&widget_mode=' + widget_mode + '&token=' + token + '&destination=' + escape(document.location), function(data) {
-      if (data.match(/^https?\:\/\/[^\/]+\/(.*)$/)) {
-        // We got a redirect.
-        document.location = data;
-      }
-      else {
-        // Replace widget by new HTML code.
-        $('#' + widget).html(data);
-        Drupal.behaviors.rate($('#' + widget));
-      }
-    });
-
-    return false;
   });
 }
