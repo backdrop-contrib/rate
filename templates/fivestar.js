dd6942a60d54205ab9072e17222173d2adfc4710
Maurits Lawende <mauritsl@243897.no-reply.drupal.org>
2011-03-06 22:05:47 +0100

    by mauritsl: Changes for better fivestar widget: 1) Add theme preprocess to better separation between theming and code. 2) Add support for template specific javascript. 3) Rewrite fivestar HTML output to a list to conform to the WCAG guidelines

---
+++ templates/fivestar.js
@@ -0,0 +1,1 @@
+// Javascript for fivestar widget.
--- templates/fivestar.tpl.php
+++ templates/fivestar.tpl.php
@@ -4,28 +4,10 @@
  * Rate widget theme
  */
 
-for ($i = 0; $i < 5; $i++) {
-  if (round($results['rating'] / 25) >= $i && $results['count']) {
-    $class = 'rate-fivestar-btn-filled';
-  }
-  else {
-    $class = 'rate-fivestar-btn-empty';
-  }
-  print theme('rate_button', $links[$i]['text'], $links[$i]['href'], $class);
-}
+print theme('item_list', $stars);
 
-if ($mode == RATE_FULL || $mode == RATE_CLOSED) {
-  // Generate the info line.
-  $info = array();
-  if ($mode == RATE_CLOSED) {
-    $info[] = t('Voting is closed.') . ' ';
-  }
-  if (isset($results['user_vote'])) {
-    $vote = round($results['user_vote'] / 25) + 1;
-    $info[] = t('You voted !vote.', array('!vote' => $vote)) . ' ';
-  }
-  $info[] = t('Total votes: !count', array('!count' => $results['count']));
-  print '<div class="rate-info">' . implode(' ', $info) . '</div>';
+if ($info) {
+  print '<div class="rate-info">' . $info . '</div>';
 }
 
 ?>
